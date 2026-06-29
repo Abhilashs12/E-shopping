91f449d5c34b475e30f91e2a314137511dbeb2c6
Abhilash <abhilashshariwal@gmail.com>
2026-06-29 15:52:54 +0530

    phase4

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 const app = express();
 
 app.use(cors());
@@ -13,8 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
-app.use("/api/categories",categoryRoutes);
+app.use("/api/categories", categoryRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/cart",cartRoutes)
+app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/payments", paymentRoutes);
 export default app;
