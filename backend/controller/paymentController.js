64a5a67161f89dfb7c63e0e4b04dba5dcd09d26b
Abhilash <abhilashshariwal@gmail.com>
2026-06-29 15:47:28 +0530

    phase2

---
+++ backend/controller/paymentController.js
@@ -0,0 +1,18 @@
+import Payment from "../models/Payment";
+
+const createPayment = async (req, res) => {
+    try {
+        const { order, amount, paymentMethod } = req.body;
+        const payment = await Payment.create({
+            user: req.user.id,
+            order,
+            amount,
+            payment,
+        });
+        res.status(201).json(payment);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
