a706a8f1e44174c79e6300bc3205b0e86f656187
Abhilash <abhilashshariwal@gmail.com>
2026-06-30 16:39:47 +0530

    phase38

--- backend/routes/addressRoutes.js
+++ backend/routes/addressRoutes.js
@@ -6,17 +6,24 @@ import {
   getSingleAddress,
   updateAddress,
   deleteAddress,
+  setDefaultAddress,
 } from "../controller/addressController.js";
 
 const router = express.Router();
-
 router.post("/", createAddress);
 
 router.get("/", getAllAddresses);
 
+// Put the specific route first
+router.put("/:id/default", setDefaultAddress);
+
+// Then generic routes
 router.get("/:id", getSingleAddress);
 
 router.put("/:id", updateAddress);
 
 router.delete("/:id", deleteAddress);
+
+export default router;
+
 export default router;
