e2c28d3f04d0005ec0a99e94ade6eaa46b0d9955
Abhilash <abhilashshariwal@gmail.com>
2026-06-30 16:52:15 +0530

    phase41

---
+++ backend/routes/wishlistRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { addToWishlist } from "../controller/wishlistController.js";
+
+const router = express.Router();
+router.post("/", addToWishlist);
+export default router;
