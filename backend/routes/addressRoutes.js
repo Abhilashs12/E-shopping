53e9a26fe99b3d7f37f98b10ce8c44ce8c9f60d8
Abhilash <abhilashshariwal@gmail.com>
2026-06-30 09:43:26 +0530

    phase28

--- backend/routes/addressRoutes.js
+++ backend/routes/addressRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 
-import { createAddress } from "../controller/addressController.js";
+import {
+  createAddress,
+  getAllAddresses,
+} from "../controller/addressController.js";
 
 const router = express.Router();
 
 router.post("/", createAddress);
+
+router.get("/", getAllAddresses);
 
 export default router;
