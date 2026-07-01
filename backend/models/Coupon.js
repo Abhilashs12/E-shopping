60bec732e8b95a9be540b7579e2e13fb6691e164
Abhilash <abhilashshariwal@gmail.com>
2026-07-01 10:39:18 +0530

    phase58

---
+++ backend/models/Coupon.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      uppercase: true,
+      trim: true,
+    },
+
+    discount: {
+      type: Number,
+      required: true,
+    },
+
+    minOrderAmount: {
+      type: Number,
+      default: 0,
+    },
+
+    expiryDate: {
+      type: Date,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Coupon = mongoose.model("Coupon", couponSchema);
+
+export default Coupon;
