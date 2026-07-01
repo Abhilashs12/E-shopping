4216574a2f10f768aa0aac27eea816f58ceef0c8
Abhilash <abhilashshariwal@gmail.com>
2026-07-01 17:10:24 +0530

    phase90

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,6 +1,23 @@
 import CategoryCard from "./CategoriesCard";
 
-const categories = [];
+const categories = [
+  {
+    title: "Fashion",
+    image: "https://images.pexels.com/photos/934070/pexels-photo-934070.jpeg",
+  },
+  {
+    title: "Electronics",
+    image: "https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg",
+  },
+  {
+    title: "Shoes",
+    image: "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg",
+  },
+  {
+    title: "Accessories",
+    image: "https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg",
+  },
+];
 
 function Categories() {
   return (
