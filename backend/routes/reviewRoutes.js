d368548afafb8d82e4712691619640a0c742c729
Abhilash <abhilashshariwal@gmail.com>
2026-07-01 09:42:29 +0530

    phase52

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { addReview } from "../controller/reviewController.js";
+import {
+  addReview,
+  getProductReviews,
+} from "../controller/reviewController.js";
 
 const router = express.Router();
 
 router.post("/", addReview);
 
+router.get("/", getProductReviews);
 export default router;
