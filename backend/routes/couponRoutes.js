8f22b70c7ac9c8a5a929247bee3835b9da97dce6
Abhilash <abhilashshariwal@gmail.com>
2026-07-01 11:12:50 +0530

    phase62

--- backend/routes/couponRoutes.js
+++ backend/routes/couponRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 
-import { createCoupon } from "../controller/couponController.js";
+import { createCoupon, getAllCoupons } from "../controller/couponController.js";
 
 const router = express.Router();
 
 router.post("/", createCoupon);
+
+router.get("/", getAllCoupons);
+
 export default router;
