9e7f28d9ce026bd08ecbcbab046b8cee52983eb3
Abhilash <abhilashshariwal@gmail.com>
2026-07-02 16:17:20 +0530

    phase107

---
+++ backend/controller/indexConteoller.js
@@ -0,0 +1,100 @@
+import Payment from "../models/Payment.js";
+
+const createPayment = async (req, res) => {
+  try {
+    const { user, order, amount, paymentMethod } = req.body;
+
+    const payment = await Payment.create({
+      user,
+      order,
+      amount,
+      paymentMethod,
+    });
+
+    res.status(201).json(payment);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getUserPayments = async (req, res) => {
+  try {
+    const payments = await Payment.find().populate("user").populate("order");
+
+    res.status(200).json(payments);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const getSinglePayment = async (req, res) => {
+  try {
+    const payment = await Payment.findById(req.params.id)
+      .populate("user")
+      .populate("order");
+
+    if (!payment) {
+      return res.status(404).json({
+        message: "Payment Not Found",
+      });
+    }
+
+    res.status(200).json(payment);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const updatePaymentStatus = async (req, res) => {
+  try {
+    const payment = await Payment.findByIdAndUpdate(
+      req.params.id,
+      {
+        paymentStatus: req.body.paymentStatus,
+      },
+      {
+        new: true,
+      },
+    );
+    if (!payment) {
+      return res.status(404).json({
+        message: "Payment not found",
+      });
+    }
+    res.status(200).json(payment);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const deletePayment = async (req, res) => {
+  try {
+    const payment = await Payment.findByIdAndDelete(req.params.id);
+
+    if (!payment) {
+      return res.status(404).json({
+        message: "Payment Not Found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Payment Deleted Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+export {
+  createPayment,
+  getUserPayments,
+  getSinglePayment,
+  updatePaymentStatus,
+  deletePayment,
+};
