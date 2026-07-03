2c2d963fb50ceff70d684f442669e75d6e0ddc28
Abhilash <abhilashshariwal@gmail.com>
2026-07-03 09:46:18 +0530

    role-based

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,22 +1,34 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-const protect =async(req,res,next)=>{
-    try{
-        const token=req.headers.authorization;
-        if(!token){
-            return res.status(401).json({
-                    message:"no token authorized",
-                })
-        }
-        const decoded = jwt.verify(token,"secretkey");
-        req.user=decoded;
-        next();
-    }
-    catch(error){
-        res.status(401).json({
-            message:"not authorized",
-        })
+const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "No token provided",
+      });
     }
 
+    const decoded = jwt.verify(token, "secretkey");
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Not authorized",
+    });
+  }
 };
+
 export default protect;
